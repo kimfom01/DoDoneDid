67bb7b463c7941aa40958832b99a1daa52f80fd3
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-11-28 21:54:54 +0300

    Add login, register and logout feature

--- TodoList/wwwroot/js/site.js
+++ TodoList/wwwroot/js/site.js
@@ -1,28 +1,26 @@
+"use strict";
+
 const uri = 'api/TodoList';
 let todos = []
 
-function getItems() {
+const getItems = () => {
     fetch(uri)
         .then(response => response.json())
         .then(data => _displayItems(data))
         .catch(error => console.error('Unable to get items.', error));
 }
 
-function addItem() {
+const addItem = () => {
     const taskTitleTextBox = document.getElementById('add-task');
 
     const todoItem = {
-        task: taskTitleTextBox.value.trim(),
-        status: 1,
+        task: taskTitleTextBox.value.trim(), status: 1,
     };
 
     fetch(uri, {
-        method: 'post',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(todoItem)
+        method: 'post', headers: {
+            'Accept': 'application/json', 'Content-Type': 'application/json'
+        }, body: JSON.stringify(todoItem)
     })
         .then(response => response.json())
         .then(() => {
@@ -32,7 +30,7 @@ function addItem() {
         .catch(error => console.error('Unable to add item.', error));
 }
 
-function deleteItem(id) {
+const deleteItem = (id) => {
     fetch(`${uri}/${id}`, {
         method: 'delete'
     })
@@ -40,7 +38,7 @@ function deleteItem(id) {
         .catch(error => console.error('Unable to delete item.', error));
 }
 
-function displayEditForm(id) {
+const displayEditForm = (id) => {
     const todoItem = todos.find(item => item.id === id);
 
     document.getElementById('edit-id').value = todoItem.id;
@@ -49,22 +47,17 @@ function displayEditForm(id) {
     document.getElementById('editForm').style.display = 'block';
 }
 
-function updateItem() {
+const updateItem = () => {
     const itemId = document.getElementById('edit-id').value;
     const status = document.getElementById('edit-status').value;
     const item = {
-        id: parseInt(itemId, 10),
-        task: document.getElementById('edit-task').value.trim(),
-        status: parseInt(status, 10),
+        id: parseInt(itemId, 10), task: document.getElementById('edit-task').value.trim(), status: parseInt(status, 10),
     };
 
     fetch(`${uri}/${itemId}`, {
-        method: 'put',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(item)
+        method: 'put', headers: {
+            'Accept': 'application/json', 'Content-Type': 'application/json'
+        }, body: JSON.stringify(item)
     })
         .then(() => getItems())
         .catch(error => console.error('Unable to update item.', error));
@@ -74,17 +67,17 @@ function updateItem() {
     return false;
 }
 
-function closeInput() {
+const closeInput = () => {
     document.getElementById('editForm').style.display = 'none';
 }
 
-function _displayCount(itemCount) {
+const _displayCount = (itemCount) => {
     const name = (itemCount === 1) ? 'task' : 'tasks';
 
     document.getElementById('counter').innerText = `${itemCount} ${name}`;
 }
 
-function _displayItems(data) {
+const _displayItems = (data) => {
     const tBody = document.getElementById('tasks');
     tBody.innerHTML = '';
 
@@ -107,13 +100,7 @@ function _displayItems(data) {
         deleteButton.setAttribute('onclick', `deleteItem(${item.id})`);
 
         let status = document.createElement("p");
-        status.classList.add(
-            "btn", 
-            `${(item.status === 1) ? "btn-success" 
-                : (item.status === 2) ? "btn-warning" 
-                    : "btn-secondary"}`, 
-            "m-0"
-        );
+        status.classList.add("btn", `${(item.status === 1) ? "btn-success" : (item.status === 2) ? "btn-warning" : "btn-secondary"}`, "m-0");
         status.innerText = item.status === 1 ? "Todo" : item.status === 2 ? "In Progress" : "Completed"
 
         let tr = tBody.insertRow();
@@ -135,3 +122,81 @@ function _displayItems(data) {
 
     todos = data;
 }
+
+const registerUser = () => {
+    const email = document.getElementById("r_email").value;
+    const password = document.getElementById("r_password").value;
+
+    const registerMessage = document.getElementById("registerMessage");
+    const registerModal = document.getElementById("registerModal");
+
+    fetch("/account/register", {
+        method: "post", headers: {
+            "Accept": "application/json", "Content-Type": "application/json"
+        }, body: JSON.stringify({
+            email, password
+        })
+    })
+        .then(response => {
+            if (response.status !== 200) {
+                const errorResponse = response.json();
+                errorResponse.then(err => {
+                    throw new Error(Object.values(err.errors).map(er => er).join(","))
+                }).catch(error => {
+                    registerMessage.innerText = error.message;
+                    console.error(error)
+                });
+            } else {
+                registerMessage.innerText = "Successfully Registered";
+                const modal = bootstrap.Modal.getInstance(registerModal);
+                setTimeout(() => {
+                    modal.hide()
+                }, 1_500)
+            }
+        });
+}
+
+const loginUser = () => {
+    const email = document.getElementById("l_email").value;
+    const password = document.getElementById("l_password").value;
+
+    const loginMessage = document.getElementById("loginMessage");
+    const loginModal = document.getElementById("loginModal");
+
+    fetch("/account/login", {
+        method: "post", headers: {
+            "Accept": "application/json", "Content-Type": "application/json"
+        }, body: JSON.stringify({
+            email, password
+        })
+    })
+        .then(response => {
+            if (response.status !== 200) {
+                throw new Error("Unable to login user.")
+            } else {
+                return response.json()
+            }
+        })
+        .then(res => {
+            loginMessage.innerText = "Login Success";
+
+            sessionStorage.setItem("tokenType", res.tokenType);
+            sessionStorage.setItem("accessToken", res.accessToken);
+            sessionStorage.setItem("expiresIn", res.expiresIn);
+            sessionStorage.setItem("refreshToken", res.refreshToken);
+
+            const modal = bootstrap.Modal.getInstance(loginModal);
+            setTimeout(() => {
+                modal.hide()
+            }, 1_500)
+        })
+        .catch(error => {
+            loginMessage.innerText = error.message;
+            console.error(error)
+        });
+}
+
+const logoutUser = () => {
+    sessionStorage.clear()
+}
+
