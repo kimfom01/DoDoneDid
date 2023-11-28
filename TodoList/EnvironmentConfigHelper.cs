a526ea284c36722daeab3e9762a0a36f6521587b
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-11-28 21:48:03 +0300

    Add config helper to get connection string from app settings and environment variable

---
+++ TodoList/EnvironmentConfigHelper.cs
@@ -0,0 +1,21 @@
+namespace TodoList;
+
+public static class EnvironmentConfigHelper
+{
+    public static string? GetConnectionString(IConfiguration config, IWebHostEnvironment env)
+    {
+        string? connectionString;
+
+        if (env.IsDevelopment())
+        {
+            connectionString = config.GetConnectionString("CONNECTION_STRING");
+        }
+        else
+        {
+            connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING")
+                               ?? throw new NullReferenceException("CONNECTION_STRING missing");
+        }
+
+        return connectionString;
+    }
+}
