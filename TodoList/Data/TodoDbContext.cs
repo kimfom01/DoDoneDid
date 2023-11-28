56e1ab88fbb82428d9621eca37014db46f75ea83
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-11-28 21:52:30 +0300

    Add seed data

--- TodoList/Data/TodoDbContext.cs
+++ TodoList/Data/TodoDbContext.cs
@@ -1,13 +1,37 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using TodoList.Models;
 
 namespace TodoList.Data;
 
-public class TodoDbContext : DbContext
+public class TodoDbContext : IdentityDbContext<User>
 {
     public DbSet<TodoItem> TodoItems { get; set; }
 
     public TodoDbContext(DbContextOptions<TodoDbContext> options) : base(options)
     {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<User>()
+            .HasData(
+                new User
+                {
+                    Id = new Guid("a24b2911-31c0-43ed-8657-4878e3abe626").ToString(),
+                    Email = "dkay4130@gmail.com"
+                });
+
+        modelBuilder.Entity<TodoItem>()
+            .HasData(
+                new TodoItem
+                {
+                    Id = 1,
+                    Status = Status.Todo,
+                    Task = "Fix Auth",
+                    UserId = new Guid("a24b2911-31c0-43ed-8657-4878e3abe626").ToString()
+                });
     }
 }
