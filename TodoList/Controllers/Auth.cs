7ad0b29e88ea81a99851c611b72502c37dc7a44c
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-12-02 16:02:08 +0300

    Add new auth endpoints to get userid and logout user

---
+++ TodoList/Controllers/Auth.cs
@@ -0,0 +1,37 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TodoList.Models;
+
+namespace TodoList.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly SignInManager<User> _signInManager;
+
+    public AuthController(SignInManager<User> signInManager)
+    {
+        _signInManager = signInManager;
+    }
+    
+    [HttpGet("user-id")]
+    public IActionResult GetUserId()
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        if (string.IsNullOrEmpty(userId))
+        {
+            return Unauthorized("User ID not found.");
+        }
+
+        return Ok(new { UserId = userId });
+    }
+
+    [HttpGet("logout")]
+    public async Task Logout()
+    {
+        await _signInManager.SignOutAsync();
+    }
+}
