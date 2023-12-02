3737e23708f3f8526c965d0865335774e22eee65
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-12-02 16:04:11 +0300

    remove token logic and modal logic

--- TodoList/wwwroot/js/auth.js
+++ TodoList/wwwroot/js/auth.js
@@ -3,9 +3,8 @@ const registerUser = () => {
     const password = document.getElementById("r_password").value;
 
     const registerMessage = document.getElementById("registerMessage");
-    const registerModal = document.getElementById("registerModal");
 
-    fetch("/account/register", {
+    fetch("/api/Auth/register", {
         method: "post", headers: {
             "Accept": "application/json", "Content-Type": "application/json"
         }, body: JSON.stringify({
@@ -24,10 +23,6 @@ const registerUser = () => {
             } else {
                 registerMessage.innerText = "Successfully Registered";
                 location.href = "/auth/login.html";
-                const modal = bootstrap.Modal.getInstance(registerModal);
-                setTimeout(() => {
-                    modal.hide()
-                }, 1_500)
             }
         });
 }
@@ -36,12 +31,9 @@ const loginUser = () => {
     const email = document.getElementById("l_email").value;
     const password = document.getElementById("l_password").value;
 
-    console.table({email, password});
-
     const loginMessage = document.getElementById("loginMessage");
-    const loginModal = document.getElementById("loginModal");
 
-    fetch("/account/login", {
+    fetch("/api/Auth/login?useCookies=true", {
         method: "post", headers: {
             "Accept": "application/json", "Content-Type": "application/json"
         }, body: JSON.stringify({
@@ -50,33 +42,16 @@ const loginUser = () => {
     })
         .then(response => {
             if (response.status !== 200) {
-                throw new Error("Unable to login user.")
+                const errorResponse = response.json();
+                errorResponse.then(err => {
+                    throw new Error(`Login ${err.detail}`)
+                }).catch(error => {
+                    loginMessage.innerText = error.message;
+                    console.error(error)
+                });
             } else {
-                return response.json()
+                loginMessage.innerText = "Login Success";
+                location.href = "/";
             }
-        })
-        .then(res => {
-            loginMessage.innerText = "Login Success";
-
-            sessionStorage.setItem("tokenType", res.tokenType);
-            sessionStorage.setItem("accessToken", res.accessToken);
-            sessionStorage.setItem("expiresIn", res.expiresIn);
-            sessionStorage.setItem("refreshToken", res.refreshToken);
-
-            location.href = "/";
-
-            const modal = bootstrap.Modal.getInstance(loginModal);
-            setTimeout(() => {
-                modal.hide()
-            }, 1_500)
-        })
-        .catch(error => {
-            loginMessage.innerText = error.message;
-            console.error(error)
         });
-}
-
-const logoutUser = () => {
-    sessionStorage.clear()
-    location.href = "/auth/login.html"
 }
