62ae1f49028f48cebcd6e893c38f26ce06547092
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-12-07 10:56:33 +0300

    Limit size of text in db

--- DoDoneDid/Models/TodoItem.cs
+++ DoDoneDid/Models/TodoItem.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DoDoneDid.Models;
 
 public class TodoItem
 {
     public int Id { get; set; }
+    [MaxLength(1000)]
     public required string Task { get; set; }
     public Status Status { get; set; }
-    public string UserId { get; set; }
+    
+    [MaxLength(100)]
+    public required string UserId { get; set; }
     public User? User { get; set; }
 }
