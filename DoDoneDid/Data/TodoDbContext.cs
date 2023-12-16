f2c8ea7cda1a5649343b3e6d8277e209c9ad85e0
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-12-16 17:39:48 +0300

    Remove auto created user and todo item

--- DoDoneDid/Data/TodoDbContext.cs
+++ DoDoneDid/Data/TodoDbContext.cs
@@ -12,28 +12,4 @@ public class TodoDbContext : IdentityDbContext<User>
     public TodoDbContext(DbContextOptions<TodoDbContext> options) : base(options)
     {
     }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
-
-        modelBuilder.Entity<User>()
-            .HasData(
-                new User
-                {
-                    Id = new Guid("a24b2911-31c0-43ed-8657-4878e3abe626").ToString(),
-                    Email = "dkay4130@gmail.com"
-                });
-
-        modelBuilder.Entity<TodoItem>()
-            .HasData(
-                new TodoItem
-                {
-                    Id = 1,
-                    Status = Status.Todo,
-                    Task = "Fix Auth",
-                    UserId = new Guid("a24b2911-31c0-43ed-8657-4878e3abe626").ToString(),
-                    DueDate = DateTime.Now
-                });
-    }
 }
