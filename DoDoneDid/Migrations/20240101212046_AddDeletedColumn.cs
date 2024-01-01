fb0a793e56e0de5fb55915b1d09d865a48d71fd8
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-01-02 00:26:28 +0300

    Update database with deleted column

---
+++ DoDoneDid/Migrations/20240101212046_AddDeletedColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DoDoneDid.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDeletedColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "TodoItems",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "TodoItems");
+        }
+    }
+}
