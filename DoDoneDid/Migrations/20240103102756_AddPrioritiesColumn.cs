0a7563d7b4c162a5652e066c071202b550eb99e5
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-01-03 13:30:20 +0300

    Add priorities column to migrations

---
+++ DoDoneDid/Migrations/20240103102756_AddPrioritiesColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DoDoneDid.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPrioritiesColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Priority",
+                table: "TodoItems",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Priority",
+                table: "TodoItems");
+        }
+    }
+}
